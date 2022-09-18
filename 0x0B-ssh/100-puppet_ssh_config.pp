8761af42fc8fef07d7b813f037bbc60de5648cae
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-09-18 22:20:40 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
-# enables connection to server without a password
-file_line {
-  'passAuth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '   PasswordAuthentication no'
-  ;
-  'keyLocation':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '   IdentityFile ~/.ssh/holberton'
+# sets up a client SSH configuration file so that we can connect to a server without using a password
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
