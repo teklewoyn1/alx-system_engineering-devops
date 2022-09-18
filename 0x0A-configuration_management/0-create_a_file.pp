4572abc1556f430c12863a4cf50e757f08e6e542
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-09-18 21:41:35 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Create a file
+# creates a file in the /tmp directory
 file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
