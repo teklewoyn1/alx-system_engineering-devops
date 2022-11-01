5f69341fa2dfcefc8a68e4635cdd5746dacdfe59
Teklewoyn Erdachew <99509917+teklewoyn1@users.noreply.github.com>
2022-11-01 23:40:18 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -17,18 +17,9 @@ def sort_histogram(histogram={}):
         else:
             histogram_dict[item[0]] = item[1]
     histogram = list(histogram_dict.items())
-    histogram.sort(
-        key=lambda kv: kv[0],
-        reverse=False
-    )
-    histogram.sort(
-        key=lambda kv: kv[1],
-        reverse=True
-    )
-    res_str = '\n'.join(list(map(
-        lambda kv: '{}: {}'.format(kv[0], kv[1]),
-        histogram
-    )))
+    histogram.sort(key=lambda kv: kv[0], reverse=False)
+    histogram.sort(key=lambda kv: kv[1], reverse=True)
+    res_str = '\n'.join(list(map(lambda kv: '{}: {}'.format(kv[0], kv[1]), histogram)))
     if res_str:
         print(res_str)
 
@@ -39,29 +30,13 @@ def count_words(subreddit, word_list, histogram=[], n=0, after=None):
     occurs in a given subreddit.
     '''
     api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
+        'Accept': 'application/json', 'User-Agent': ' '.join(['Mozilla/5.0 (Windows NT 10.0; Win64; x64)', 'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71', 'Safari/537.36', 'Edg/97.0.1072.62'])}
     sort = 'hot'
     limit = 30
     res = requests.get(
-        '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
-            'https://www.reddit.com',
-            subreddit,
-            sort,
-            limit,
-            n,
-            after if after else ''
-        ),
-        headers=api_headers,
-        allow_redirects=False
-    )
+        '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format('https://www.reddit.com', subreddit, sort, limit,  n, after if after else ''),
+        headers=api_headers, allow_redirects=False)
     if not histogram:
         word_list = list(map(lambda word: word.lower(), word_list))
         histogram = list(map(lambda word: (word, 0), word_list))
@@ -69,21 +44,9 @@ def count_words(subreddit, word_list, histogram=[], n=0, after=None):
         data = res.json()['data']
         posts = data['children']
         titles = list(map(lambda post: post['data']['title'], posts))
-        histogram = list(map(
-            lambda kv: (kv[0], kv[1] + sum(list(map(
-                lambda txt: txt.lower().split().count(kv[0]),
-                titles
-            )))),
-            histogram
-        ))
+        histogram = list(map(lambda kv: (kv[0], kv[1] + sum(list(map(lambda txt: txt.lower().split().count(kv[0]), titles)))), histogram))
         if len(posts) >= limit and data['after']:
-            count_words(
-                subreddit,
-                word_list,
-                histogram,
-                n + len(posts),
-                data['after']
-            )
+            count_words(subreddit, word_list, histogram, n + len(posts), data['after'])
         else:
             sort_histogram(histogram)
     else:
